1786ba5a5a258224db5ed2aec2db16c314b7f240
abhi-kadam-2302 <Abhi.Kadam@lntinfotech.com>
2020-11-18 12:53:56 +0530

    Case Study Added

---
+++ CaseStudy1/App.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+
+namespace CaseStudy1
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+
+        internal Student(int Id, string Name, string Date)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.DateOfBirth = Date;
+        }
+
+    }
+
+    class Info
+    {
+        public void Display(Student student)
+        {
+            Console.WriteLine("Student ID : {0}", student.Id);
+            Console.WriteLine("Student Name : {0}", student.Name);
+            Console.WriteLine("Student BirthDate : {0}", student.DateOfBirth);
+            Console.WriteLine();
+        }
+
+        public void Display(Course course)
+        {
+            Console.WriteLine("Course ID : {0}", course.Id);
+            Console.WriteLine("Course Name : {0}", course.Name);
+            Console.WriteLine("Course Duration : {0}", course.Duration);
+            Console.WriteLine("Course Fees : {0}", course.Fees);
+            Console.WriteLine();
+        }
+    }
+
+    class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public int Fees { get; set; }
+
+        internal Course(int Id, string Name, int Duration, int Fees)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Duration = Duration;
+            this.Fees = Fees;
+        }
+    }
+
+    class App
+    {
+        void Scenario1()
+        {
+            Student student = new Student(1, "Anna", "23-09-1998");
+            new Student(2, "Wayne", "12-02-1997");
+
+            Console.WriteLine("------------------Scenario 1--------------------");
+
+            Info info = new Info();
+            info.Display(student);            
+        }
+
+        void Scenario2()
+        {
+            Student[] studentarray = new Student[3];
+            studentarray[0] = new Student(101, "Sara", "12-06-1989");
+            studentarray[1] = new Student(102, "Sean", "31-04-1999");
+            studentarray[2] = new Student(103, "Clara", "01-01-1909");
+
+            Console.WriteLine("------------------Scenario 2--------------------");
+            
+            Info info = new Info();
+            foreach (Student student in studentarray)
+                info.Display(student);
+        }
+
+        void scenario3()
+        {
+            int n, Id;
+            string Name, Date;
+            Console.Write("Enter number of students : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Student[] studentarray = new Student[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter Id {0} : ", (i+1));
+                Id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Name {0} : ", (i + 1));
+                Name = Console.ReadLine();
+                Console.Write("Enter BirthDate {0} : ", (i + 1));
+                Date = Console.ReadLine();
+                Console.WriteLine();
+                studentarray[i] = new Student(Id, Name, Date);
+            }
+
+            Console.WriteLine("------------------Scenario 3--------------------");
+
+            Info info = new Info();
+            foreach (Student student in studentarray)
+                info.Display(student);
+        }
+
+        void scenario4()
+        {
+            Course course = new Course(10001, "Computer", 4, 100000);
+
+            Console.WriteLine("------------------Scenario 4--------------------");
+            
+            Info info = new Info();
+            info.Display(course);
+
+        }
+
+        static void Main()
+        {
+            App app = new App();
+            app.Scenario1();            
+            app.Scenario2();            
+            app.scenario3();            
+            app.scenario4();
+
+            Console.Read();
+        }
+    }
+}
+
+
---
+++ CaseStudy1/CowBull.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaseStudy1
+{
+    class CowBull
+    {
+        public static string GuessWord;
+        public static char[] Buffer;
+        public static char[] GuessWordArray;
+        public char Guess;
+        public int Attempts = 10, Cows = 0, Bulls = 0, Pos = 0;
+        public bool flag = true;
+
+        void Input()
+        {
+            Console.WriteLine("******************* PLAYER 1 *******************");
+            Console.Write("Enter a word for Player 2 to guess : ");
+            GuessWord = Console.ReadLine();
+            Buffer = new char[GuessWord.Length];
+            GuessWordArray = GuessWord.ToCharArray();
+            Console.WriteLine();
+            Console.WriteLine("******************* PLAYER 2 *******************");
+            Console.WriteLine("Player 2 gets 10 Attempts to guess {0} lettered word !", GuessWord.Length);
+            Console.WriteLine();
+        }
+
+        void Calculation()
+        {
+            while (flag)
+            {
+                Console.WriteLine();
+                Console.Write("PLayer 2 enter a guess : ");
+                Guess = Convert.ToChar(Console.ReadLine());
+                Attempts--;
+                if (GuessWord.Contains(Guess))
+                {
+                    Bulls++;
+                    Buffer[Pos++] = Guess;
+                    if (GuessWordArray.SequenceEqual(Buffer))
+                    {
+                        Console.WriteLine("Congratulations ! You guessed the word '{0}' !", GuessWord);
+                        Console.WriteLine("Player 2 Wins !");
+                        flag = false;
+                    }
+                    else
+                        Console.WriteLine("You have guessed a correct letter ! {0} Attempts remaining !", Attempts);
+                }
+                else
+                {
+                    Cows++;
+                    Console.WriteLine("OOPS ! Incorrect Guess ! Guess Again ! {0} Attempts remaining !", Attempts);
+                    if (Attempts == 0)
+                        flag = false;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        void Display()
+        {
+            if (Attempts == 0 && Bulls != GuessWord.Length)
+                Console.WriteLine("Player 1 Wins !");
+            Console.WriteLine();
+            Console.WriteLine("Cows : {0}, Bulls : {1}", Cows, Bulls);
+        }
+
+        static void Main()
+        {
+            CowBull cowBull = new CowBull();
+            cowBull.Input();
+            cowBull.Calculation();
+            cowBull.Display();        
+
+            Console.ReadLine();
+        }
+    }
+}
